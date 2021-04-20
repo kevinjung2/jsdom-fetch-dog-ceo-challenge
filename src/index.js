439afd695de08d4e0dd47ae6121fd547365e1867
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-20 08:55:50 -0400

    commit tree 0a25491941fe406222f812f0774b8891a6bb75fb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp => resp.json())
+  .then(json => for (let image in json) {
+    let img = document.addElement('img')
+    img.innerHTML = image
+  })
+})
